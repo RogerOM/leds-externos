b430b8072e95311b065c1983a3797ec6f0c8ea9b
Roger O <43441351+RogerOM@users.noreply.github.com>
2021-03-03 18:19:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+function green (núm: number) {
+    pins.digitalWritePin(DigitalPin.P1, núm)
+}
+input.onButtonPressed(Button.A, function () {
+    red(1)
+    green(1)
+    blue(0)
+    basic.pause(1000)
+    red(0)
+    green(1)
+    blue(1)
+    basic.pause(1000)
+    red(1)
+    green(0)
+    blue(1)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    red(randint(0, 1))
+    green(randint(0, 1))
+    blue(randint(0, 1))
+})
+function blue (núm: number) {
+    pins.digitalWritePin(DigitalPin.P2, núm)
+}
+function red (núm: number) {
+    pins.digitalWritePin(DigitalPin.P0, núm)
+}
+red(0)
+green(0)
+blue(0)
