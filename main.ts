5b5dae72880a57fbfd7d5f180c3809af25ed2985
Roger O <43441351+RogerOM@users.noreply.github.com>
2021-03-03 18:28:01 +0100

    colore terciarios al azar

--- main.ts
+++ main.ts
@@ -1,47 +1,48 @@
 function green (núm: number) {
-    pins.digitalWritePin(DigitalPin.P1, núm)
+    pins.analogWritePin(AnalogPin.P1, núm)
 }
 // colores primarios RGB
 input.onButtonPressed(Button.A, function () {
-    red(1)
+    red(1023)
     green(0)
     blue(0)
     basic.pause(1000)
     red(0)
-    green(1)
+    green(1023)
     blue(0)
     basic.pause(1000)
     red(0)
     green(0)
-    blue(1)
+    blue(1023)
     basic.pause(1000)
 })
+// al azar
 input.onButtonPressed(Button.AB, function () {
-    red(randint(0, 1))
-    green(randint(0, 1))
-    blue(randint(0, 1))
+    red(randint(0, 1023))
+    green(randint(0, 1023))
+    blue(randint(0, 1023))
 })
-// colores secundarios amarillo-cyan-magenta
+// colores terciarios - combinaciones de ejemplo de las muchas que hay
 input.onButtonPressed(Button.B, function () {
-    red(1)
-    green(1)
+    red(500)
+    green(1000)
     blue(0)
     basic.pause(1000)
     red(0)
-    green(1)
-    blue(1)
+    green(500)
+    blue(1000)
     basic.pause(1000)
-    red(1)
+    red(1000)
     green(0)
-    blue(1)
+    blue(500)
     basic.pause(1000)
 })
 function blue (núm: number) {
-    pins.digitalWritePin(DigitalPin.P2, núm)
+    pins.analogWritePin(AnalogPin.P2, núm)
 }
 function red (núm: number) {
-    pins.digitalWritePin(DigitalPin.P0, núm)
+    pins.analogWritePin(AnalogPin.P0, núm)
 }
-red(1)
-green(1)
-blue(1)
+red(randint(0, 1023))
+green(randint(0, 1023))
+blue(randint(0, 1023))
